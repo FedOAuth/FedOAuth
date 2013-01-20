d6a5dfcfee2a32e54a987a85302b1ec5c5e4dbdb
Patrick Uiterwijk <puiterwijk@gmail.com>
2013-01-20 20:00:18 +0100

    First part of openid should be working

--- fas_openid/default_config.py
+++ fas_openid/default_config.py
@@ -3,12 +3,9 @@
 import os
 
 FAS_HTTPS_REQUIRED = False
-
+OPENID_ENDPOINT = 'http://localhost:5000'
+OPENID_IDENTITY_URL = 'http://localhost:5000/id/%s'
 SECRET_KEY = 'SECRET_KEY'
-# url to the database server:
 DB_URL = 'sqlite:////tmp/fas_openid_dev.sqlite'
-
-
-# Path to the alembic configuration file
 PATH_ALEMBIC_INI = os.path.join(os.path.dirname(os.path.abspath(__file__)),
     '..', 'alembic.ini')
--- fas_openid/views.py
+++ fas_openid/views.py
@@ -4,12 +4,46 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from model import FASOpenIDStore
 
 from fas_openid import APP as app, FAS
+from fas_openid.model import FASOpenIDStore
 
 from flask_fas import fas_login_required
 
+import openid
+from openid.extensions import sreg
+from openid.server.server import Server as openid_server
+from openid.server import server
+from openid.consumer import discover
+
+
+def get_server():
+    if not hasattr(g, 'openid_server'):
+        g.openid_server = openid_server(FASOpenIDStore(), op_endpoint=app.config['OPENID_ENDPOINT'])
+    return g.openid_server
+
+
 @app.route('/')
 def view_main():
-    return render_template('index.html', text='main')
+    try:
+        openid_request = get_server().decodeRequest(request.args)
+    except server.ProtocolError, openid_error:
+        return openid_respond(openid_error)
+
+    if openid_request is None:
+        return render_template('index.html', text='MAIN PAGE, no OpenID request')
+    elif openid_request.mode in ['checkid_immediate', 'checkid_setup']:
+        return 'TODO'
+        pass    # TODO: CHECK THE REQUEST
+    else:
+        return openid_respond(get_server().handleRequest(openid_request))
+
+def openid_respond(response):
+    try:
+        webresponse = self.openid.encodeResponse(openid_response)
+        return (webresponse.body, webresponse.status, webresponse.headers)
+    except server.EncodingError, why:
+        headers = {'Content-type': 'text/plain; charset=UTF-8'} 
+        return why.response.encodeToKVForm(), 400, headers
+
 
 @app.route('/logout/')
 def auth_logout():
@@ -21,20 +55,21 @@ def auth_logout():
 
 @app.route('/login/', methods=['GET','POST'])
 def auth_login():
-    if not 'next' in request.values:
+    if not 'next' in request.args and not 'next' in session:
         return redirect(url_for('view_main'))
-    nextpage = request.values['next']
+    if 'next' in request.args:
+        session['next'] = request.args['next']
     if g.fas_user:
-        return redirect(nextpage)
+        return redirect(session['next'])
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         result = FAS.login(username, password)
         if result:
-            return redirect(nextpage)
+            return redirect(session['next'])
         else:
             flash('Incorrect username or password')
-    return render_template('login.html', nextpage=nextpage)
+    return render_template('login.html')
 
 @app.route('/test/')
 @fas_login_required

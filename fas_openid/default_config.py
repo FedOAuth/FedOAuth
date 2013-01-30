84aa400615bc0c8263330342c41226ecc9f99444
Patrick Uiterwijk <puiterwijk@gmail.com>
2013-01-30 14:11:16 +0100

    Secure by default

--- fas_openid/default_config.py
+++ fas_openid/default_config.py
@@ -10,8 +10,8 @@ NON_TRUSTED_ROOTS = []
 
 LOGIN_URL = 'http://localhost:5000/login/'
 
-FAS_HTTPS_REQUIRED = False
+FAS_HTTPS_REQUIRED = True
 OPENID_ENDPOINT = 'http://localhost:5000'
-OPENID_IDENTITY_URL = 'http://localhost:5000/id/%s'
+OPENID_IDENTITY_URL = 'http://localhost:5000/id/%s/'
 SECRET_KEY = 'SECRET_KEY'
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/fas_openid_dev.sqlite'

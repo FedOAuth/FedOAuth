6306f1a2cb485dfa5cc72015e980d5c741b43ae9
Patrick Uiterwijk <puiterwijk@gmail.com>
2013-02-20 07:31:04 +0100

    Only provide CLA when asked

--- fas_openid/views.py
+++ fas_openid/views.py
@@ -136,7 +136,7 @@ def addTeams(request, response, groups):
 
 def addCLAs(request, response, cla_uris):
     cla_req = cla.CLARequest.fromOpenIDRequest(request)
-    if not cla_req:
+    if not cla_req.wereCLAsRequested:
         return
     cla_resp = cla.CLAResponse.extractResponse(cla_req, cla_uris)
     response.addExtension(cla_resp)

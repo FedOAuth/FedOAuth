876a8ced39d32a92c11e5b8009f24b813a72d6ca
Patrick Uiterwijk <puiterwijk@gmail.com>
2013-04-27 01:40:20 +0200

    Try some indenting

--- fas_openid/auth_fas.py
+++ fas_openid/auth_fas.py
@@ -85,8 +85,8 @@ def auth_login():
         get_session().save()
     if logged_in() and not \
         ('timeout' in get_session() and get_session()['timeout']):
-        # We can also have "timeout" as of 0.4.0
-        # indicating PAPE or application configuration requires a re-auth
+            # We can also have "timeout" as of 0.4.0
+            # indicating PAPE or application configuration requires a re-auth
             log_debug('Info', {
                 'message': 'User tried to login but is already authenticated'})
             return redirect(get_session()['next'])
@@ -95,25 +95,25 @@ def auth_login():
         password = request.form['password']
         if (not app.config['AVAILABLE_FILTER']) or \
             (username in app.config['AVAILABLE_TO']):
-            user = check_login(username, password)
-            if user:
-                log_info('Success', {
-                    'username': username,
-                    'message': 'User authenticated succesfully'})
-                user = user.toDict()  # A bunch is not serializable...
-                user['groups'] = [x['name'] for x in
-                        user['approved_memberships']]
-                get_session()['user'] = user
-                get_session()['last_auth_time'] = time()
-                get_session()['timeout'] = False
-                get_session()['trust_root'] = ''
-                get_session().save()
-                return redirect(get_session()['next'])
-            else:
-                log_warning('Failure', {
-                    'username': username,
-                    'message': 'User entered incorrect username or password'})
-                flash(_('Incorrect username or password'))
+                user = check_login(username, password)
+                if user:
+                    log_info('Success', {
+                        'username': username,
+                        'message': 'User authenticated succesfully'})
+                    user = user.toDict()  # A bunch is not serializable...
+                    user['groups'] = [x['name'] for x in
+                            user['approved_memberships']]
+                    get_session()['user'] = user
+                    get_session()['last_auth_time'] = time()
+                    get_session()['timeout'] = False
+                    get_session()['trust_root'] = ''
+                    get_session().save()
+                    return redirect(get_session()['next'])
+                else:
+                    log_warning('Failure', {
+                        'username': username,
+                        'message': 'User entered incorrect username or password'})
+                    flash(_('Incorrect username or password'))
         else:
             log_warning('Failure', {
                 'username': username,

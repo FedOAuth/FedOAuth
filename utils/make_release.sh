6447b9165ebc603777f77d5f2feda42ab940392d
Patrick Uiterwijk <puiterwijk@gmail.com>
2013-05-01 15:55:12 +0200

    Update make_release.sh to delete the branch after tagging

--- utils/make_release.sh
+++ utils/make_release.sh
@@ -33,7 +33,8 @@ tx pull -a
 git add fas_openid/translations
 git commit -m "Updated translations"
 git tag -s v$version -m "Release v$version"
-git commit -d make-release
+git branch -d make-release
+git checkout master
 git push origin v$version
 git archive --format=tar --prefix=fas-openid-$version/ HEAD | gzip > release/fas-openid-$version.tar.gz
 (

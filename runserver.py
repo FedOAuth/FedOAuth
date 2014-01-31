7329a647e6aae071677cc0cf4a81690cdc785857
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-01-31 12:58:21 +0100

    Create DBs on runserver

--- runserver.py
+++ runserver.py
@@ -5,5 +5,9 @@ __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
 from fas_openid import APP
+from fas_openid import model
+
+model.create_tables(APP.config['SQLALCHEMY_DATABASE_URI'], True)
+
 APP.debug = True
 APP.run()

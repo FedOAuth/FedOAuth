a6f3d8a86e994f7e3d93871ba1ee48258bd268eb
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-02-06 10:29:31 +0100

    fixlogout

--- fedoauth/model.py
+++ fedoauth/model.py
@@ -58,7 +58,8 @@ class DBSession(db.Model, SessionMixin, DictMixin):
 
     @classmethod
     def open_session(cls, app, request):
-        if not request.path == '/' and not get_auth_module().is_dynamic_content(request.path):
+        if not request.path == '/' and not request.path == '/logout/' 
+                and not get_auth_module().is_dynamic_content(request.path):
             return None
         request.path
         sessionid = request.cookies.get('sessionid')

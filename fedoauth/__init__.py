dd282bc35549198ee015b67c7853abb49b5c6353
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-02-07 23:09:11 +0100

    Fix licensing

--- fedoauth/__init__.py
+++ fedoauth/__init__.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 #-*- coding: UTF-8 -*-
+# Copyright (C) 2014 Patrick Uiterwijk <puiterwijk@gmail.com>
+#
+# This file is part of FedOAuth.
+#
+# FedOAuth is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FedOAuth is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FedOAuth.  If not, see <http://www.gnu.org/licenses/>.
 
 ## These two lines are needed to run on EL6
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
--- fedoauth/views.py
+++ fedoauth/views.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2014 Patrick Uiterwijk <puiterwijk@gmail.com>
+#
+# This file is part of FedOAuth.
+#
+# FedOAuth is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FedOAuth is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FedOAuth.  If not, see <http://www.gnu.org/licenses/>.
 from flask import redirect, url_for, render_template, flash
 from flaskext.babel import gettext as _
 

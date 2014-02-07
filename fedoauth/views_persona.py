2164dff0ae72132d01a30c06d3305562e38a75e1
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-02-07 21:54:45 +0100

    Update field names

--- fedoauth/views_persona.py
+++ fedoauth/views_persona.py
@@ -65,10 +65,10 @@ if key and key_e and key_n:
 
         claim = {}
         # Valid for at most 24 hours
-        claim['issuedAt'] = 1000 * int(time.time() - 10)
-        claim['expiresAt'] = 1000 * int(time.time() + \
+        claim['iat'] = 1000 * int(time.time() - 10)
+        claim['exp'] = 1000 * int(time.time() + \
                                     min(certDuration, 24 * 60 * 60))
-        claim['issuer'] = app.config['PERSONA_DOMAIN']
+        claim['iss'] = app.config['PERSONA_DOMAIN']
         claim['public-key'] = json.loads(publicKey)
         claim['principal'] = {'email': email}
 

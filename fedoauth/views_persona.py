7f0e96698dbf80b934416c386f0cd0f52feccb0e
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-02-07 21:46:20 +0100

    fix

--- fedoauth/views_persona.py
+++ fedoauth/views_persona.py
@@ -65,8 +65,9 @@ if key and key_e and key_n:
 
         claim = {}
         # Valid for at most 24 hours
-        claim['issuedAt'] = int(time.time())
-        claim['expiresAt'] = int(time.time() + min(certDuration, 24 * 60 * 60))
+        claim['issuedAt'] = 1000 * int(time.time())
+        claim['expiresAt'] = 1000 * int(time.time() + \
+                                    min(certDuration, 24 * 60 * 60))
         claim['issuer'] = app.config['PERSONA_DOMAIN']
         claim['public-key'] = json.loads(publicKey)
         claim['principal'] = {'email': email}
